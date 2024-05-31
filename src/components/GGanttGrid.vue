b19e8798a713805544e60103fcbba633ddf403ac
marco.mandelli <marco.mandelli@elmec.it>
2024-05-31 23:52:37 +0200

    feat: Added highlightDates default weekends

--- src/components/GGanttGrid.vue
+++ src/components/GGanttGrid.vue
@@ -7,7 +7,7 @@
       :style="{
         width,
         background:
-          highlightDates?.includes(dayjs(date).format('YYYY-MM-DD')) ||
+          highlightDatesTemp?.includes(dayjs(date).format('YYYY-MM-DD')) ||
           highlightedUnits?.includes(Number(value))
             ? colors.hoverHighlight
             : undefined
@@ -21,13 +21,42 @@ import provideConfig from "../provider/provideConfig.js"
 import useTimeaxisUnits from "../composables/useTimeaxisUnits.js"
 import dayjs from "dayjs"
 
-defineProps<{
+const props = defineProps<{
   highlightedUnits?: number[]
   highlightDates?: string[]
 }>()
 
 const { colors } = provideConfig()
 const { timeaxisUnits } = useTimeaxisUnits()
+
+function getWeekendDays(startDate, endDate) {
+  // Converti le date di input in oggetti dayjs
+  const start = dayjs(startDate);
+  const end = dayjs(endDate);
+
+  // Array per memorizzare i giorni del weekend
+  const weekendDays = [];
+
+  // Itera sui giorni nell'intervallo
+  let current = start;
+  while (current.isBefore(end) || current.isSame(end, 'day')) {
+    // Controlla se il giorno corrente è sabato (6) o domenica (0)
+    if (current.day() === 6 || current.day() === 0) {
+      weekendDays.push(current.format('YYYY-MM-DD'));
+    }
+    // Passa al giorno successivo
+    current = current.add(1, 'day');
+  }
+
+  return weekendDays;
+}
+
+let highlightDatesTemp: string[]
+
+const startDate = dayjs(timeaxisUnits.value.upperUnits[0].date).format("YYYY-MM-DD");
+const endDate = dayjs(timeaxisUnits.value.upperUnits[1].date).format("YYYY-MM-DD");
+const weekends = getWeekendDays(startDate, endDate);
+highlightDatesTemp = [...weekends, ...props.highlightDates]
 </script>
 
 <style>
